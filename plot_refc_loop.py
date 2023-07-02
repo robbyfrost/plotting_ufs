c237645fe196bda386ab4253b68ee3761bbc9e7e
robbyfrost <robby@ou.edu>
2023-07-02 23:49:00 +0000

    refc plots complete, sbcape plot in progress

--- plot_refc_loop.py
+++ plot_refc_loop.py
@@ -18,15 +18,15 @@ from plotting_functions import read_grib
 # important parameters
 
 # directory where hrrr grib data are located
-dgrib_h = "/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2019052000_3km_hrrrphys/2019052000/postprd/"
+dgrib_h = "/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2023041900_3km_hrrrphys/2023041900/postprd/"
 # directory where rap grib data are located
-dgrib_r = "/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2019052000_3km_rapphys/2019052000/postprd/"
+dgrib_r = "/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2023041900_3km_rapphys/2023041900/postprd/"
 # natlev or prslev
 nat_prs = "prslev"
 # message number for composite reflectivity
 mn_refc = 40
 # directory for figure to be output
-figdir = "/scratch2/BMC/fv3lam/Robby.Frost/figures/20190520/refc/"
+figdir = "/scratch2/BMC/fv3lam/Robby.Frost/figures/20230419/refc/"
 # --------------------------------
 # read in data
 
@@ -34,8 +34,8 @@ figdir = "/scratch2/BMC/fv3lam/Robby.Frost/figures/20190520/refc/"
 refch_all = []
 refcr_all = []
 # start and end hours
-hri = 33 # first hour to be plot
-hrf = 37 # enter last hour + 1
+hri = 1 # first hour to be plot
+hrf = 5 # enter last hour + 1
 # loop over forecast hours of interest
 for hr in range(hri,hrf):
     refc_h, lat, lon, valid_date = read_grib(hr, dgrib_h, nat_prs, mn_refc)
@@ -59,17 +59,21 @@ print(f"Creating 4 x 2 Reflectivity plot")
 fig, ax = plt.subplots(ncols=2, nrows=4, subplot_kw={'projection': ccrs.PlateCarree()}, 
                        figsize=(9,16.7), constrained_layout=True)
 # mapping
-plt_area = [-104, -94, 30, 39] # W, E, S, N
+geoData = gpd.read_file('https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson')
+plt_area = [-101, -94, 33.5, 37.5] # W, E, S, N
 for i, iax in enumerate(ax[:,0]):
     iax.coastlines()
     iax.add_feature(cpf.BORDERS)
     iax.add_feature(cpf.STATES)
     iax.set_extent(plt_area)
+    geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
 for i, iax in enumerate(ax[:,1]):
     iax.coastlines()
     iax.add_feature(cpf.BORDERS)
     iax.add_feature(cpf.STATES)
     iax.set_extent(plt_area)
+    # Load the json file with county coordinates
+    geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
 
 # plot HRRR
 c0 = ax[0,0].contourf(lon, lat, refch_all[0].values, clevs, 
