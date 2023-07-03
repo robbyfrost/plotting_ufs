c344dde046098ad3292dc8ddc1de22c7cce778f1
robbyfrost <robby@ou.edu>
2023-07-03 19:50:14 +0000

    Updates to plot_refc* and plot_sbcape*

--- plot_refc_loop.py
+++ plot_refc_loop.py
@@ -9,11 +9,13 @@
 # and looping over the forecast
 # --------------------------------
 import matplotlib.pyplot as plt
+from matplotlib import rc
 import cartopy.crs as ccrs
 import cartopy.feature as cpf
 import numpy as np
 from metpy.plots import ctables
 from plotting_functions import read_grib
+import geopandas as gpd
 # --------------------------------
 # important parameters
 
@@ -27,89 +29,155 @@ nat_prs = "prslev"
 mn_refc = 40
 # directory for figure to be output
 figdir = "/scratch2/BMC/fv3lam/Robby.Frost/figures/20230419/refc/"
+# flag to plot 4 hour refc
+four_hr = False
+# flag to plot single hour refc
+single_hr = True
 # --------------------------------
-# read in data
-
-# create lists to store data
-refch_all = []
-refcr_all = []
-# start and end hours
-hri = 1 # first hour to be plot
-hrf = 5 # enter last hour + 1
-# loop over forecast hours of interest
-for hr in range(hri,hrf):
-    refc_h, lat, lon, valid_date = read_grib(hr, dgrib_h, nat_prs, mn_refc)
-    refc_r = read_grib(hr, dgrib_r, nat_prs, mn_refc, array_only=True)
-    # append hourly refc array to list
-    refch_all.append(refc_h)
-    refcr_all.append(refc_r)
+# plotting setup
+rc('font',weight='normal',size=15)
+# rc('text',usetex='True')
+rc('figure',facecolor='white')
 # --------------------------------
-# Side by side reflectivity plot
+# Side by side 4 hour reflectivity plot
+if four_hr:
+    # read in data
 
-# Define your custom colorbar bounds
-cbar_min = 0
-cbar_max = 75.1
-# set reflectivity levels to be plotted
-clevs = np.arange(cbar_min, cbar_max, 5)
-# define color table using metpy colortables
-colors = ctables.registry.get_colortable('NWSReflectivity')
+    # create lists to store data
+    refch_all = []
+    refcr_all = []
+    # start and end hours
+    hri = 1 # first hour to be plot
+    hrf = 5 # enter last hour + 1
+    # loop over forecast hours of interest
+    for hr in range(hri,hrf):
+        refc_h, lat, lon, valid_date = read_grib(hr, dgrib_h, nat_prs, mn_refc)
+        refc_r = read_grib(hr, dgrib_r, nat_prs, mn_refc, array_only=True)
+        # append hourly refc array to list
+        refch_all.append(refc_h)
+        refcr_all.append(refc_r)
+    # --------------------------------
+    # Plot
 
-# create plot
-print(f"Creating 4 x 2 Reflectivity plot")
-fig, ax = plt.subplots(ncols=2, nrows=4, subplot_kw={'projection': ccrs.PlateCarree()}, 
-                       figsize=(9,16.7), constrained_layout=True)
-# mapping
-geoData = gpd.read_file('https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson')
-plt_area = [-101, -94, 33.5, 37.5] # W, E, S, N
-for i, iax in enumerate(ax[:,0]):
-    iax.coastlines()
-    iax.add_feature(cpf.BORDERS)
-    iax.add_feature(cpf.STATES)
-    iax.set_extent(plt_area)
-    geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
-for i, iax in enumerate(ax[:,1]):
-    iax.coastlines()
-    iax.add_feature(cpf.BORDERS)
-    iax.add_feature(cpf.STATES)
-    iax.set_extent(plt_area)
-    # Load the json file with county coordinates
-    geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
+    # Define your custom colorbar bounds
+    cbar_min = 0
+    cbar_max = 75.1
+    # set reflectivity levels to be plotted
+    clevs = np.arange(cbar_min, cbar_max, 5)
+    # define color table using metpy colortables
+    colors = ctables.registry.get_colortable('NWSReflectivity')
 
-# plot HRRR
-c0 = ax[0,0].contourf(lon, lat, refch_all[0].values, clevs, 
-                    transform=ccrs.PlateCarree(), cmap=colors)
-c2 = ax[1,0].contourf(lon, lat, refch_all[1].values, clevs, 
-                    transform=ccrs.PlateCarree(), cmap=colors)
-c4 = ax[2,0].contourf(lon, lat, refch_all[2].values, clevs, 
-                    transform=ccrs.PlateCarree(), cmap=colors)
-c6 = ax[3,0].contourf(lon, lat, refch_all[3].values, clevs, 
-                    transform=ccrs.PlateCarree(), cmap=colors)
-# plot RAP
-c1 = ax[0,1].contourf(lon, lat, refcr_all[0].values, clevs,
-                    transform=ccrs.PlateCarree(), cmap=colors)
-c3 = ax[1,1].contourf(lon, lat, refcr_all[1].values, clevs,
-                    transform=ccrs.PlateCarree(), cmap=colors)
-c5 = ax[2,1].contourf(lon, lat, refcr_all[2].values, clevs,
-                    transform=ccrs.PlateCarree(), cmap=colors)
-c7 = ax[3,1].contourf(lon, lat, refcr_all[3].values, clevs,
-                    transform=ccrs.PlateCarree(), cmap=colors)
+    # create plot
+    print(f"Creating 4 x 2 Reflectivity plot")
+    fig, ax = plt.subplots(ncols=2, nrows=4, subplot_kw={'projection': ccrs.PlateCarree()}, 
+                        figsize=(9,16.7), constrained_layout=True)
+    # mapping
+    geoData = gpd.read_file('https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson')
+    plt_area = [-101, -94, 33.5, 37.5] # W, E, S, N
+    for i, iax in enumerate(ax[:,0]):
+        iax.coastlines()
+        iax.add_feature(cpf.BORDERS)
+        iax.add_feature(cpf.STATES)
+        iax.set_extent(plt_area)
+        geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
+    for i, iax in enumerate(ax[:,1]):
+        iax.coastlines()
+        iax.add_feature(cpf.BORDERS)
+        iax.add_feature(cpf.STATES)
+        iax.set_extent(plt_area)
+        # Load the json file with county coordinates
+        geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
 
-# add axes titles
-for i, iax in enumerate(ax[:,0]):
-    iax.set_title(f"HRRR Valid at {refch_all[i].validDate} UTC")
-for i, iax in enumerate(ax[:,1]):
-    iax.set_title(f"RAP Valid at {refcr_all[i].validDate} UTC")
+    # plot HRRR
+    c0 = ax[0,0].contourf(lon, lat, refch_all[0].values, clevs, 
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    c2 = ax[1,0].contourf(lon, lat, refch_all[1].values, clevs, 
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    c4 = ax[2,0].contourf(lon, lat, refch_all[2].values, clevs, 
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    c6 = ax[3,0].contourf(lon, lat, refch_all[3].values, clevs, 
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    # plot RAP
+    c1 = ax[0,1].contourf(lon, lat, refcr_all[0].values, clevs,
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    c3 = ax[1,1].contourf(lon, lat, refcr_all[1].values, clevs,
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    c5 = ax[2,1].contourf(lon, lat, refcr_all[2].values, clevs,
+                        transform=ccrs.PlateCarree(), cmap=colors)
+    c7 = ax[3,1].contourf(lon, lat, refcr_all[3].values, clevs,
+                        transform=ccrs.PlateCarree(), cmap=colors)
 
-# Add colorbar
-cbar = fig.colorbar(c0, ax=ax, orientation='horizontal', extend=True, pad=0.01, fraction=0.017, aspect=30)
-cbar.set_label('Simulated Composite Reflectivty [dBZ]')
+    # add axes titles
+    for i, iax in enumerate(ax[:,0]):
+        iax.set_title(f"HRRR Valid at {refch_all[i].validDate} UTC")
+    for i, iax in enumerate(ax[:,1]):
+        iax.set_title(f"RAP Valid at {refcr_all[i].validDate} UTC")
 
-# tight_layout
-plt.tight_layout
+    # Add colorbar
+    cbar = fig.colorbar(c0, ax=ax, orientation='horizontal', extend=True, pad=0.01, fraction=0.017, aspect=30)
+    cbar.set_label('Simulated Composite Reflectivty [dBZ]')
 
-# save figure
-figdir_full = f"{figdir}refc_4x2_f{hri}-f{hrf-1}.png"
-print(f"Saving figure to {figdir_full}")
-plt.savefig(f"{figdir_full}")
-plt.close()
-print(f"Finished plotting!")
+    # tight_layout
+    plt.tight_layout
+
+    # save figure
+    figdir_full = f"{figdir}refc_4x2_f{hri}-f{hrf-1}.png"
+    print(f"Saving figure to {figdir_full}")
+    plt.savefig(figdir_full)
+    plt.close()
+    print("Finished plotting! \n")
+# --------------------------------
+# Side by side 1 hour reflectivity
+
+if single_hr:
+    # loop over forecast hours of interest
+    for hr in range(37):
+        print(f"Creating Reflectivity Plot for Hour {hr}")
+        refc_h, lat, lon, valid_date = read_grib(hr, dgrib_h, nat_prs, mn_refc)
+        refc_r = read_grib(hr, dgrib_r, nat_prs, mn_refc, array_only=True)
+
+        # Define your custom colorbar bounds
+        cbar_min = 0
+        cbar_max = 75.1
+        # set reflectivity levels to be plotted
+        clevs = np.arange(cbar_min, cbar_max, 5)
+        # define color table using metpy colortables
+        colors = ctables.registry.get_colortable('NWSReflectivity')
+
+        # create plot
+        fig, ax = plt.subplots(ncols=2, subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(16,6), constrained_layout=True)
+
+        # plot HRRR
+        c0 = ax[0].contourf(lon, lat, refc_h.values, clevs, 
+                            transform=ccrs.PlateCarree(), 
+                            cmap=colors)
+        # plot RAP
+        c1 = ax[1].contourf(lon, lat, refc_r.values, clevs,
+                            transform=ccrs.PlateCarree(), 
+                            cmap=colors)
+
+        # mapping
+        plt_area = [-101, -94, 33.5, 37.5] # W, E, S, N
+        for i, iax in enumerate(ax):
+            iax.coastlines()
+            iax.add_feature(cpf.BORDERS)
+            iax.add_feature(cpf.STATES)
+            iax.set_extent(plt_area)
+            # Load the json file with county coordinates
+            geoData = gpd.read_file('https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson')
+            geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
+
+        # add axes titles
+        ax[0].set_title(f"HRRR F0{hr},  Valid {valid_date} UTC")
+        ax[1].set_title(f"RAP F0{hr},  Valid {valid_date} UTC")
+
+        # Add colorbar
+        cbar = fig.colorbar(c0, ax=ax, orientation='horizontal', extend=True, pad=0.05, aspect=50)
+        cbar.set_label('Simulated Composite Reflectivty [dBZ]')
+
+        # save figure
+        figdir_full = f"{figdir}refc_sidebyside_f{hr}.png"
+        print(f"Saving figure to {figdir_full}")
+        plt.savefig(figdir_full)
+        plt.close()
+        print("Finished plotting! \n")
--- plot_sbcape_loop.py
+++ plot_sbcape_loop.py
@@ -1,120 +1,137 @@
 # --------------------------------
-# Name: plot_sbcape_loop.py
+# Name: plot_sbcape.ipynb
 # Author: Robert M. Frost
 # NOAA Global Systems Laboratory
-# Created: 23 June 2023
-# Purpose: Loop to plot surface based
-# cape comparisons at different times 
-# during forecast runs
+# Created: 03 July 2023
+# Purpose: Plot surface based cape 
+# utilizing read_grib from 
+# plotting_functions.py
 # --------------------------------
-import xarray as xr
 import matplotlib.pyplot as plt
+from matplotlib import rc
 import cartopy.crs as ccrs
 import cartopy.feature as cpf
-from datetime import datetime
-from matplotlib import rc
-import seaborn
 import numpy as np
+from metpy.plots import ctables
+from plotting_functions import read_grib
+import geopandas as gpd
+import seaborn
 # --------------------------------
-for hr in range(7, 37):
-    # read in rap and hrrr data
-    if hr < 10:
-        dnc_h = f"/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2019052000_3km_hrrrphys/2019052000/postprd/rrfs.t00z.natlev.f00{hr}.rrfs_conuscompact_3km.grib2"
-        dnc_r = f"/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2019052000_3km_rapphys/2019052000/postprd/rrfs.t00z.natlev.f00{hr}.rrfs_conuscompact_3km.grib2"
-    else:
-        dnc_h = f"/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2019052000_3km_hrrrphys/2019052000/postprd/rrfs.t00z.natlev.f0{hr}.rrfs_conuscompact_3km.grib2"
-        dnc_r = f"/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2019052000_3km_rapphys/2019052000/postprd/rrfs.t00z.natlev.f0{hr}.rrfs_conuscompact_3km.grib2"
-    hrrr = xr.open_dataset(dnc_h,
-                        engine="cfgrib",
-                        filter_by_keys={'stepType': 'instant', 'typeOfLevel': 'surface'})
-    rap = xr.open_dataset(dnc_r,
-                        engine="cfgrib",
-                        filter_by_keys={'stepType': 'instant', 'typeOfLevel': 'surface'})
-    
-    # set lats and lons
-    lat = hrrr.latitude
-    lon = hrrr.longitude
+# important parameters
 
-    # take numpy.datatime64 and put into datetime format
-    valid_time_str = str(hrrr.time.values + hrrr.time.step.values)
-    valid_time_str = valid_time_str[:19]
-    valid_time = datetime.strptime(valid_time_str, '%Y-%m-%dT%H:%M:%S')
-
-    # plotting setup
-    rc('font',weight='normal',size=15)#,family='serif',serif='Times New Roman')
-    # rc('text',usetex='True')
-    rc('figure',facecolor='white')
-
+# first and last hour to be plotted
+hri = 0
+hrf = 37 # last hour + 1
+# directory where hrrr grib data are located
+dgrib_h = "/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2023041900_3km_hrrrphys/2023041900/postprd/"
+# directory where rap grib data are located
+dgrib_r = "/scratch2/BMC/fv3lam/Robby.Frost/expt_dirs/2023041900_3km_rapphys/2023041900/postprd/"
+# natlev or prslev
+nat_prs = "natlev"
+# message number for SBCAPE
+mn_refc = 1408
+# directory for figure to be output
+figdir = "/scratch2/BMC/fv3lam/Robby.Frost/figures/20230419/sbcape/"
+# --------------------------------
+# plotting setup
+rc('font',weight='normal',size=12.5)
+# rc('text',usetex='True')
+rc('figure',facecolor='white')
+# --------------------------------
+for hr in range(hri,hrf):
+    print(f"Hour {hr}")
+    # read in data
+    sbcape_h, lat, lon, valid_date = read_grib(hr, dgrib_h, nat_prs, mn_refc)
+    sbcape_r = read_grib(hr, dgrib_r, nat_prs, mn_refc, array_only=True)
     # --------------------------------
-    # side by side sbcape plot
+    # Side by side sbcape plot
+    print(f"Creating 1 x 2 SBCAPE Plot")
 
     # Define your custom colorbar bounds
-    cbar_min = 100
-    cbar_max = 5000
+    cbar_min = 0
+    cbar_max = 4501
     clevs = np.arange(cbar_min, cbar_max, 100)
     # color palette
     colors = "CMRmap_r"
 
     # create plot
-    fig, ax = plt.subplots(ncols=2, subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(18,10))
+    fig, ax = plt.subplots(ncols=2, subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(16,6), constrained_layout=True)
+
+    # plot HRRR
+    c0 = ax[0].contourf(lon, lat, sbcape_h.values, clevs, 
+                        transform=ccrs.PlateCarree(), 
+                        cmap=colors)
+    # plot RAP
+    c1 = ax[1].contourf(lon, lat, sbcape_r.values, clevs, 
+                        transform=ccrs.PlateCarree(), 
+                        cmap=colors)
+
     # mapping
-    plt_area = [-104, -94, 30, 39] # W, E, S, N
+    plt_area = [-101, -94, 33.5, 37.5] # W, E, S, N
     for i, iax in enumerate(ax):
         iax.coastlines()
         iax.add_feature(cpf.BORDERS)
         iax.add_feature(cpf.STATES)
         iax.set_extent(plt_area)
-    # plot
-    c0 = ax[0].contourf(lon, lat, hrrr.cape, clevs, 
-                        transform=ccrs.PlateCarree(), 
-                        cmap=colors,
-                        extend="both")
-    c1 = ax[1].contourf(lon, lat, rap.cape, clevs,
-                        transform=ccrs.PlateCarree(), 
-                        cmap=colors,
-                        extend="both")
-    # pretty up
-    ax[0].set_title(f"HRRR F0{hr},  Valid {valid_time} UTC")
-    ax[1].set_title(f"RAP F0{hr},  Valid {valid_time} UTC")
+        # Load the json file with county coordinates
+        geoData = gpd.read_file('https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson')
+        geoData.plot(ax=iax, color="none", lw=0.3, aspect=1)
+
+    # set title
+    ax[0].set_title(f"HRRR F0{hr},  Valid {valid_date} UTC")
+    ax[1].set_title(f"RAP F0{hr},  Valid {valid_date} UTC")
+
     # Add colorbar
-    cbar = fig.colorbar(c1, ax=ax, orientation='horizontal', extend=True, pad=0.05, aspect=50)
+    cbar = fig.colorbar(c1, ax=ax, orientation='horizontal', extend=True, pad=0.03, aspect=50)
     cbar.set_label('SBCAPE [J kg$^{-1}$]')
+
     # save and close figure
-    plt.savefig(f"/scratch2/BMC/fv3lam/Robby.Frost/figures/20190520/sbcape_sidebyside_f{hr}.png")
+    figdir_full = f"{figdir}sbcape_sidebyside_f{hr}.png"
+    print(f"Saving figure to {figdir_full}")
+    plt.savefig(figdir_full)
     plt.close()
-     # --------------------------------
-    # sbcape difference plot
+    print("Finished plotting 1 x 2 SBCAPE!")
+    # --------------------------------
+    # SBCAPE Difference Plot
+    print("Creating SBCAPE Difference Plot")
 
     # Define your custom colorbar bounds
-    cbar_min = -3500
-    cbar_max = 3500
-    clevs = np.arange(cbar_min, cbar_max, 100)
-
-    # Create a normalized colormap
-    cdiff = seaborn.color_palette("seismic", as_cmap=True)
+    cbar_min = -1000.1
+    cbar_max = 1000.1
+    clevs = np.linspace(cbar_min, cbar_max, 51)
+    # color palette
+    colors = seaborn.color_palette("seismic", as_cmap=True)
 
     # create plot
-    fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(10,10))
+    fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, 
+                        figsize=(10,6.5), constrained_layout=True)
+
+    # plot HRRR - RAP
+    c0 = ax.contourf(lon, lat, sbcape_h.values - sbcape_r.values,
+                    clevs, transform=ccrs.PlateCarree(),
+                    cmap=colors, extend="both")
 
     # mapping
-    plt_area = [-104, -94, 30, 39] # W, E, S, N
+    plt_area = [-101, -94, 33.5, 37.5] # W, E, S, N
     ax.coastlines()
     ax.add_feature(cpf.BORDERS)
     ax.add_feature(cpf.STATES)
     ax.set_extent(plt_area)
-    # plot
-    c0 = ax.contourf(lon, lat, hrrr.cape - rap.cape, clevs, 
-                    transform=ccrs.PlateCarree(), 
-                    cmap=cdiff, 
-                    vmin=cbar_min, vax=cbar_max,
-                    extend="both")
+    # Load the json file with county coordinates
+    geoData = gpd.read_file('https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson')
+    geoData.plot(ax=ax, color="none", lw=0.3, aspect=1)
 
-    # pretty up
-    ax.set_title(f"HRRR - RAP F0{hr},  Valid {valid_time} UTC")
+    # set title
+    ax.set_title(f"HRRR - RAP F0{hr},  Valid {valid_date} UTC")
 
     # Add colorbar
-    cbar = fig.colorbar(c0, ax=ax, orientation='horizontal', extend=True, pad=0.05, aspect=50)
+    cbar = fig.colorbar(c0, ax=ax, orientation='horizontal', extend=True, pad=0.03, aspect=50)
     cbar.set_label('SBCAPE [J kg$^{-1}$]')
+    cbar.set_ticks(np.arange(cbar_min, cbar_max, 250))
 
-    plt.savefig(f"/scratch2/BMC/fv3lam/Robby.Frost/figures/20190520/sbcape_diff_f{hr}.png")
+    # save and close figure
+    figdir_full = f"{figdir}sbcape_diff_f{hr}.png"
+    print(f"Saving figure to {figdir_full}")
+    plt.savefig(figdir_full)
     plt.close()
+    print(f"Finished with hour {hr}! \n")
